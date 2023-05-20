e157b90b34c623c87bc49381d4525fd10c6da393
¨raystef66¨ <s.vanbarel@gmail.com>
2023-05-20 08:54:18 +0200

    InfiniR Cepheus v1.3 * merge v4.14.315

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 KERNEL_DEFCONFIG=cepheus_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
-FINAL_KERNEL_ZIP=InfiniR_cepheus_A13_v1.2.zip
+FINAL_KERNEL_ZIP=InfiniR_cepheus_A13_v1.3.zip
 
 # paths
 TC="/home/raystef66/kernel/prebuilts"
