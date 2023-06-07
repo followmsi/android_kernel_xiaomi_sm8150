b95ef6137f217357d2ba007bf160ac5cc7dbe0d4
¨raystef66¨ <s.vanbarel@gmail.com>
2023-06-07 12:57:58 +0200

    InfiniR Cepheus v1.5 * * update to latest kernelSU v10984 (use min. v0.6.0 manager)

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 KERNEL_DEFCONFIG=cepheus_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
-FINAL_KERNEL_ZIP=InfiniR_cepheus_A13_v1.4.zip
+FINAL_KERNEL_ZIP=InfiniR_cepheus_A13_v1.5.zip
 
 # paths
 TC="/home/raystef66/kernel/prebuilts"
